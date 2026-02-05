25c2059df969a55056a2859b5da6df2bdd402be6
Rusty Rain <2069201+rainliu@users.noreply.github.com>
2026-02-05 17:22:50 +0000

    make it works

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
+use hello_remote_world::add;
 
 fn main() {
     let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    let c = add(1, 2);
+    println!("Hello, {} with {}!", name, c);
 }
